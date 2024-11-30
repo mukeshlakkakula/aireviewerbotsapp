f2db546df83c716999273b2bbfe28701246ed8f5
Mukesh <lakkakulababblu@gmail.com>
2024-11-30 09:47:16 +0530

    resolved build issues

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { useState } from "react";
-import { loginUser } from "../../utils/appwrite";
+import { loginUser } from "@/utils/appwrite";
 
 export default function Login() {
   const [email, setEmail] = useState("");
--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { useState } from "react";
-import { registerUser } from "../../utils/appwrite";
+import { registerUser } from "@/utils/appwrite";
 
 export default function Register() {
   const [email, setEmail] = useState("");
